13c8e0c3bbe5a9509e67a7eb20c22e1788142d0d
suhyeon4820 <bananacco@naver.com>
2023-03-06 23:31:48 +0900

    player 이동 및 공격 애니메이션

--- OJT/Assets/02_Scripts/Character/PlayerController.cs
+++ OJT/Assets/02_Scripts/Character/PlayerController.cs
@@ -7,15 +7,18 @@ public class PlayerController : MonoBehaviour
     private InputSystem playerInput;
     private CharacterController controller;
     private Vector3 playerVelocity;
-    private bool groundedPlayer;
-    [SerializeField] private float playerSpeed = 2.0f;
-    [SerializeField] private float jumpHeight = 1.0f;
-    [SerializeField] private float gravityValue = -9.81f;
+    //private bool groundedPlayer;
+    [SerializeField] private float playerSpeed = 3.0f;
+
+    private Animator animator;
+    //[SerializeField] private float gravityValue = -9.81f;
 
     private void Awake()
     {
         playerInput = new InputSystem();
         controller = GetComponent<CharacterController>();
+
+        animator = transform.GetChild(0).GetComponent<Animator>();
     }
 
     private void OnEnable()
@@ -29,39 +32,37 @@ public class PlayerController : MonoBehaviour
     }
 
 
-
-
-    private void Start()
-    {
-        
-    }
-
     void Update()
     {
-        groundedPlayer = controller.isGrounded;
-        if (groundedPlayer && playerVelocity.y < 0)
-        {
-            playerVelocity.y = 0f;
-        }
+        PlayerMove();
+        PlayerAttack();
+    }
 
+    void PlayerMove()
+    {
         Vector2 movementInput = playerInput.Player.Move.ReadValue<Vector2>();
-        Vector3 move = new Vector3(movementInput.x, 0f, movementInput.y);
+        Vector3 movementValue = new Vector3(movementInput.x, 0f, movementInput.y);
         //Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-        controller.Move(move * Time.deltaTime * playerSpeed);
+        controller.Move(movementValue * Time.deltaTime * playerSpeed);
 
-        if (move != Vector3.zero)
+        if (movementValue != Vector3.zero)
         {
-            gameObject.transform.forward = move;
-            Debug.Log("move");
+            animator.SetBool("Walk", true);
+            gameObject.transform.forward = movementValue;
         }
+        else
+        {
+            animator.SetBool("Walk", false);
+        }
+    }
 
-        //// Changes the height position of the player..
-        //if (Input.GetButtonDown("Jump") && groundedPlayer)
-        //{
-        //    playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
-        //}
-
-        playerVelocity.y += gravityValue * Time.deltaTime;
-        controller.Move(playerVelocity * Time.deltaTime);
+    void PlayerAttack()
+    {
+        if(playerInput.Player.Attack.triggered)
+        {
+            //
+            animator.SetTrigger("Attack");
+        }
+        
     }
 }
